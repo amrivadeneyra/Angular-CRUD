3985fb9bbf38555b302ff07098b7e768473f9ca4
manuel <arivadeneyrai@autonoma.edu.pe>
2023-02-13 09:16:19 -0500

    crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-crud';
+  title: string = 'CRUD con Angular';
+
+  message: string = '';
+
+  employees = [
+    { 'name': 'Anderson', position: 'Trainee', email: 'anderson@mail.com' },
+    { 'name': 'Manuel', position: 'Trainee', email: 'manuel@mail.com' }
+  ];
+
+  model: any = {}; //Contendra los datos temporales
+
+  model2: any = {}; // para actualizar cambios
+
+  hideUpdate: boolean = true;
+
+  addEmployee(): void {
+    this.employees.push(this.model);
+    this.model = {};
+    this.message = 'Added field';
+
+  }
+
+  deleteEmployee(i: any): void {
+    //s
+    let answer = confirm('Se eliminará... Estas seguro?');
+    if (answer) {
+      this.employees.splice(i, 1);
+      this.message = 'Field removed'
+    }
+  }
+  myValue: any;
+  editEmployee(i: any): void {
+    this.hideUpdate = false;
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+  }
+
+  updateEmployee(): void {
+    this.hideUpdate = true;
+    let i = this.myValue;
+    for (let j = 0; j < this.employees.length; j++) {
+      if (i === j) {
+        this.employees[i] = this.model2
+        this.message = 'Updated field';
+        this.model2 = {}; //limpiar el formulario 
+      }
+    }
+  }
+  closeAlert() {
+    //closeAlert
+    this.message = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms'
+
 import { AppComponent } from './app.component';
+import { CreateDataComponent } from './create-data/create-data.component';
+import { ReadDataComponent } from './read-data/read-data.component';
+import { UpdateDataComponent } from './update-data/update-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateDataComponent,
+    ReadDataComponent,
+    UpdateDataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
